d4e0067d10f140bb6081b36603dbc64512c9c0d0
kevin morrison <kevin@forestgiant.com>
2017-03-14 18:03:20 -0400

    fixed toggle js

--- static/javascripts/codetoggle.js
+++ static/javascripts/codetoggle.js
@@ -1,18 +1,40 @@
 $(document).ready(function() {
 
-//makes Go tab active
-$(".tabs-thumb:first-of-type").addClass("is-active").closest(".tabs").find(".tabs-panel:first-of-type").show();
+	var param = window.location.search;
 
-//on click, shows Node tab and hides Go tab
-$(".tabs-thumb:last-of-type").click(function() {
-  $(this).removeClass("is-active").closest(".tabs").find(".tabs-panel:first-of-type").hide();
-  $(this).addClass("is-active").closest(".tabs").find(".tabs-panel:last-of-type").show();
-});
+	//todo: fix this to parse params
+	if (param == "?toggle=node")
+	{
+		//makes Node tab active
+		$(".tabs-thumb:last-of-type").addClass("is-active").closest(".tabs").find(".tabs-panel:last-of-type").show();
 
-// on click, shows Go tab and hides Node tab
-$(".tabs-thumb:first-of-type").click(function() {
-  $(this).removeClass("is-active").closest(".tabs").find(".tabs-panel:last-of-type").hide();
-  $(this).addClass("is-active").closest(".tabs").find(".tabs-panel:first-of-type").show();
-});
+	} else {
+
+		//makes Go tab active
+		$(".tabs-thumb:first-of-type").addClass("is-active").closest(".tabs").find(".tabs-panel:first-of-type").show();
+	
+	}
+	
+	//on click, shows Node tab and hides Go tab
+	$(".tabs-thumb:last-of-type").click(function() {
+		
+		$(this).closest(".tabs").children().find(".tabs-panel:first-of-type").hide();
+	  	$(this).siblings().removeClass("is-active");
+
+	  	$(this).addClass("is-active");
+	  	$(this).closest(".tabs").children().find(".tabs-panel:last-of-type").show();
+
+	});
+
+	// on click, shows Go tab and hides Node tab
+	$(".tabs-thumb:first-of-type").click(function() {
+	  
+	  	$(this).closest(".tabs").children().find(".tabs-panel:last-of-type").hide();
+	  	$(this).siblings().removeClass("is-active");
+
+	  	$(this).addClass("is-active");
+	  	$(this).closest(".tabs").children().find(".tabs-panel:first-of-type").show();
+
+	});
 
 });
